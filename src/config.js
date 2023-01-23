baf9acf17e79ed566902f38879ed97fd05f52fac
Alfonso <adeunab@gmail.com>
2023-01-23 15:52:16 +0100

    changed db_host

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 3000
-export const DB_HOST = process.env.DB_HOST || 'dpg-cf56i0la4992g5gcfm1g-a'
+export const DB_HOST = process.env.DB_HOST || 'dpg-cf56i0la4992g5gcfm1g-a.frankfurt-postgres.render.com'
 export const DB_NAME = process.env.DB_NAME || 'userdb_ugoq'
 export const DB_USER = process.env.DB_USER || 'userdb_ugoq_user'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'CZyvKuB5aTIcquDfjQGgHXHzWryIGz5I' 
